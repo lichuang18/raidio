c80eabb3f98fa8e6da7deb09fc4ff747d8aedd3b
lichuang18 <983459843@qq.com>
2025-06-09 15:40:58 +0800

    version v1.0

--- src/libaio_run.c
+++ src/libaio_run.c
@@ -58,117 +58,47 @@ void *worker(void *arg)
 {
     thread_ctx_t *ctx = (thread_ctx_t *)arg;
     long long offset = 0;
-    int i;
     u_int64_t nr_requests = ctx->size / ctx->bs;
+    u_int64_t count_get = 0;
     double start = get_time_usec();
+    int inflight = 0;
     while (nr_requests > ctx->submitted) {
         int index = ctx->submitted % ctx->qd;
         struct iocb *iocb = &ctx->iocbs[index];
-
-        io_prep_pwrite(iocb, ctx->fd, ctx->buffers[index], ctx->bs, offset);
-        ctx->iocb_ptrs[index] = iocb;
-        io_submit(ctx->ctx, 1, &ctx->iocb_ptrs[index]);
-        ctx->submitted++;
-        offset += ctx->bs * 1024;//
-
-        if (ctx->submitted % ctx->qd == 0) {
-            int ret = io_getevents(ctx->ctx, 1, ctx->qd, ctx->events, NULL);
-            for (i = 0; i < ret; i++)
-                ctx->bytes_done += ctx->events[i].res;
+        // printf("nr/sub %llu/%llu,inf %d,b_done %llu,offset %ld\n",nr_requests, count_get,inflight,ctx->bytes_done,offset);
+        if(inflight < ctx->qd){
+            io_prep_pwrite(iocb, ctx->fd, ctx->buffers[index], ctx->bs, offset);
+            ctx->iocb_ptrs[index] = iocb;
+            int subs = io_submit(ctx->ctx, 1, &ctx->iocb_ptrs[index]);
+            if(subs > 0){
+                ctx->submitted +=subs;
+                inflight +=subs;
+                offset += ctx->bs;
+            }else{
+                printf("io_submit error...\n");
+            }
+        } else {//if (inflight >= ctx->qd) { //
+            int ret = io_getevents(ctx->ctx, 1, 1, ctx->events, NULL);
+            if((ret == 1) && (long)ctx->events[0].res > 0){
+                ctx->bytes_done += ctx->events[0].res;
+            }
+            if ((long)ctx->events[0].res < 0) {
+                continue;
+            }else{
+                count_get++;
+                --inflight;
+            }
         }
     }
-
-    // 收尾：收集所有未完成请求
-    if (ctx->submitted % ctx->qd != 0) {
-        int remaining = ctx->submitted % ctx->qd;
-        int ret = io_getevents(ctx->ctx, remaining, remaining, ctx->events, NULL);
-        for (i = 0; i < ret; i++)
-            ctx->bytes_done += ctx->events[i].res;
-    }
+    int remaining = ctx->submitted - count_get;
+    
+    int ret = io_getevents(ctx->ctx, remaining, remaining, ctx->events, NULL);
+    if(ret != remaining)
+        printf("tail io_getevents error[%d]\n",ret);
     double end = get_time_usec();
-    ctx->elapsed_us = end - start; // 转换为毫秒
+    ctx->elapsed_us = end - start;
     return NULL;
 }
-
-// void *io_thread(void *arg) {
-//     thread_ctx_t *ctx = (thread_ctx_t *)arg;
-//     u_int64_t offset = 0;//ctx->offset_start;
-//     struct timespec start, end;
-//     u_int64_t nr_requests = ctx->size / ctx->bs;
-//     srand(time(NULL) + pthread_self());
-
-//     clock_gettime(CLOCK_REALTIME, &start);
-//     while(nr_requests > ctx->submitted){ //还有请求未提交就继续执行
-//         // u_int64_t blk_index;
-//         // if (strcmp(ctx->rw, "randread") == 0 || strcmp(ctx->rw, "randwrite") == 0) {
-//         //     blk_index = rand() % (nr_requests);
-//         // } else {//顺序读写
-//         //     blk_index = ctx->submitted + 1;
-//         // }
-//         // u_int64_t offset = ctx->offset_start + blk_index * ctx->bs;
-//         int index = ctx->submitted % ctx->qd ;
-//         struct iocb *iocb = &ctx->iocbs[index];
-
-//         if (strcmp(ctx->rw, "read") == 0 || strcmp(ctx->rw, "randread") == 0){
-//             io_prep_pread(iocb, ctx->fd, ctx->buffers[index], ctx->bs, offset);
-//         } else{
-//             io_prep_pwrite(iocb, ctx->fd, ctx->buffers[index], ctx->bs, offset);
-//         }
-//         ctx->iocb_ptrs[index] = iocb;
-//         int ret = io_submit(ctx->ctx, 1, &ctx->iocb_ptrs[index]);
-
-//         if (ret == 1) {
-//             ctx->submitted++;
-//         } else if (ret < 0) {
-//             perror("io_submit failed");
-//             break;
-//         }
-//         if (ctx->submitted % ctx->qd == 0) {
-//             int ret = io_getevents(ctx->ctx, 1, ctx->qd, ctx->events, NULL);
-//             for (int i = 0; i < ret; i++)
-//                 ctx->bytes_done += ctx->events[i].res;
-//         }
-//     }
-
-//     if (ctx->submitted % ctx->qd != 0) { // 收尾：收集所有未完成请求
-//         int remaining = ctx->submitted % ctx->qd;
-//         int ret = io_getevents(ctx->ctx, 1, remaining, ctx->events, NULL);
-//         for (int i = 0; i < ret; i++){
-//             ctx->bytes_done += ctx->events[i].res;
-//         }
-//     } 
-//         // if(LOG_LAT){
-//         //     for (int i = 0; i < ret; ++i) {
-//         //         struct iocb *cb = events[i].obj;
-//         //         // 找到对应 req 的索引
-//         //         int req_idx = -1;
-//         //         for (size_t j = 0; j < ios_to_submit; ++j) {
-//         //             if (&reqs[j].iocb == cb) {
-//         //                 req_idx = j;
-//         //                 break;
-//         //             }
-//         //         }
-//         //         if (req_idx != -1) {
-//         //             struct timespec done_ts;
-//         //             clock_gettime(CLOCK_REALTIME, &done_ts);
-//         //             uint64_t start_ns = reqs[req_idx].submit_ts.tv_sec * 1000000000ULL + reqs[req_idx].submit_ts.tv_nsec;
-//         //             uint64_t end_ns = done_ts.tv_sec * 1000000000ULL + done_ts.tv_nsec;
-//         //             uint64_t latency_ns = end_ns - start_ns;
-//         //             int bucket = latency_ns / 1000; //每 1us 一个桶
-//         //             if (bucket >= BUCKETS) bucket = BUCKETS - 1;
-//         //             ctx->latency_hist[bucket]++;
-//         //             ctx->total_ios++;
-//         //         }
-//         //     }
-//         // } 
-//     clock_gettime(CLOCK_REALTIME, &end);
-//     u_int64_t start_ns = start.tv_sec * 1000000000L + start.tv_nsec;
-//     u_int64_t end_ns = end.tv_sec * 1000000000L + end.tv_nsec;
-//     ctx->elapsed_ms = (end_ns - start_ns) / 1e6; // 转换为毫秒
-//     // ctx->submitted = submitteds;
-//     pthread_exit(NULL);
-// }
-
 
 int libaio_run(struct rio_args *args) {
     u_int64_t bs = args->block_size;
@@ -181,7 +111,6 @@ int libaio_run(struct rio_args *args) {
     pthread_t *threads = malloc(num_threads * sizeof(pthread_t));
     thread_ctx_t *contexts = malloc(num_threads * sizeof(thread_ctx_t));
 
-    double start = get_time_usec();
     for (int i = 0; i < num_threads; i++) {
         strncpy(contexts[i].file, args->file, sizeof(contexts[i].file) - 1);
         contexts[i].file[sizeof(contexts[i].file) - 1] = '\0'; // 确保结尾安全
@@ -223,22 +152,19 @@ int libaio_run(struct rio_args *args) {
     }
     for (int i = 0; i < num_threads; i++) {
         pthread_join(threads[i], NULL);
-        // free(contexts[i].iocbs);
-        // free(contexts[i].iocb_ptrs);
-        // free(contexts[i].events);
-        // for (int j = 0; j < qd; j++)
-        //     free(contexts[i].buffers[j]);
-        // close(contexts[i].fd);
-        // io_destroy(contexts[i].ctx);
     }
-
-    double end = get_time_usec();
-    long long total_bytes = contexts[0].bytes_done;// + td[1].bytes_done;
-    //double elapsed_sec = (end - start) / 1e6;
-    printf("Total bytes written: %" PRIu64 "\n", contexts[0].bytes_done);
-    printf("Elapsed time: %.2f seconds\n", contexts[0].elapsed_us / 1e6);
-    printf("Bandwidth: %.2f MB/s\n", contexts[0].bytes_done / contexts[0].elapsed_us / (1024 * 1024));
-
+    u_int64_t total_bytes = 0;
+    double use_time = 0, max_time = 0;
+    for(int i = 0; i < num_threads; i++){
+        total_bytes += contexts[i].bytes_done;// + td[1].bytes_done;
+        use_time = contexts[i].elapsed_us / 1e6; //s
+        if (use_time > max_time)
+            max_time = use_time;
+    }
+    double bw_perf_mb = (double)total_bytes / max_time / (1024 * 1024);
+    // printf("Total bytes written: %" PRIu64 "\n", total_bytes);
+    // printf("Elapsed time: %.2f seconds\n", use_time);
+    // printf("Bandwidth: %.2f MB/s\n",  bw_perf_mb);
     for (int i = 0; i < num_threads; i++) {
         // pthread_join(threads[i], NULL);
         free(contexts[i].iocbs);
@@ -252,7 +178,6 @@ int libaio_run(struct rio_args *args) {
    
     // u_int64_t total_io = 0;
     // double max_time = 0, total_time = 0;
-    
     // for (int i = 0; i < num_threads; i++) {
     //     total_io += contexts[i].bytes_done;
     //     total_time += contexts[i].elapsed_ms;
@@ -261,31 +186,26 @@ int libaio_run(struct rio_args *args) {
     // }
     // double mb = (double)total_io / 1024.0 / 1024.0;
 
-
-    // char log_file[256];
-    // if (args->fk_plot == FAST_PLOT_NONE) {
-    //     char log_file[256];
-    //     snprintf(log_file, sizeof(log_file), "result/raid-normal-results.log");
-    //     printf("\n== %s Summary  ==\n", args->rw_type);
-    //     printf("Threads   : %d |  qd  : %d |  bs  : %" PRIu64 "KB\n", num_threads, qd, bs/1024);
-    //     printf("Total MB  : %.4f MB\n", mb);
-    //     printf("Max Time  : %.4f sec\n", max_time/1000);
-    //     printf("Total BW  : %.2f MB/s\n\n", mb / max_time * 1000);
-    // } else {
-    //     snprintf(log_file, sizeof(log_file), "result/raid-%s-results.log", fast_plot_names[args->fk_plot]);
-    // }
-    // FILE *fp1 = fopen(log_file, "a");  // 追加写
-
-    // if (fp1) {
-    //     struct timespec ts;
-    //     clock_gettime(CLOCK_REALTIME, &ts);  // 获取当前真实时间
-    //     //fprintf(fp1, "%s, %d, %" PRIu64 " , %d, %.2f\n",args->rw_type, args->iodepth, args->block_size/1024, args->thread_n, mb / max_time * 1000);
-    //     long long nanoseconds = (long long)ts.tv_sec * 1000000000LL + ts.tv_nsec;//, %lld
-    //     fprintf(fp1, "%s, %d, %" PRIu64 " , %d, %.2f, %lld\n",args->rw_type, args->iodepth, args->block_size/1024, args->thread_n, mb / max_time * 1000, nanoseconds);
-    //     fclose(fp1);
-    // }
-
-    
+    char log_file[256];
+    if (args->fk_plot == FAST_PLOT_NONE) {
+        char log_file[256];
+        snprintf(log_file, sizeof(log_file), "result/raid-normal-results.log");
+        printf("\n========= %s Summary  =========\n", args->rw_type);
+        printf("Threads: [%d] qd: [%d] bs: [%" PRIu64 "KB]\n", num_threads, qd, bs/1024);
+        printf("Total bytes written: %" PRIu64 "\n", total_bytes);
+        printf("Elapsed time: %.2f seconds\n", use_time);
+        printf("Bandwidth: %.2f MB/s\n",  bw_perf_mb);
+    } else {
+        snprintf(log_file, sizeof(log_file), "result/raid-%s-results.log", fast_plot_names[args->fk_plot]);
+    }
+    FILE *fp1 = fopen(log_file, "a");  // 追加写
+    if (fp1) {
+        struct timespec ts;
+        clock_gettime(CLOCK_REALTIME, &ts);  // 获取当前真实时间
+        long long nanoseconds = (long long)ts.tv_sec * 1000000000LL + ts.tv_nsec;//, %lld
+        fprintf(fp1, "%s, %d, %" PRIu64 " , %d, %.2f, %lld\n",args->rw_type, qd, bs/1024, args->thread_n, bw_perf_mb, nanoseconds);
+        fclose(fp1);
+    }
     free(threads);
     free(contexts);
     
